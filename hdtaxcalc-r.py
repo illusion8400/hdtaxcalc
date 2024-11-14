2cbe70594949be23124f9a138012840483cc20e5
illusion <illusion840@gmail.com>
2024-11-13 22:08:30 -0700

    2.1.1

--- hdtaxcalc-r.py
+++ hdtaxcalc-r.py
@@ -54,6 +54,11 @@ def main(self, entry1, entry2, entry3, checkbox1):
 def about():
     print(f"HDTAXCALC v{version.version}\nNov 2024 - illusion")
 
+def clear_all(self):
+    self.entry1.delete(0, 'end')
+    self.entry2.delete(0, 'end')
+    self.entry3.delete(0, 'end')
+    
 class App:
     def __init__(self, root):
         # Minimal tkinter app
@@ -88,6 +93,9 @@ class App:
         # We add a button to test our setup
         self.go_button = tk.Button(self.root, text="Go", command=lambda: main(self,self.entry1.get(), self.entry2.get(), self.entry3.get(), self.checkbox1_var.get()))
         self.go_button.pack(side="top")
+        # Clear All
+        self.clear_button = tk.Button(self.root, text="Clear", command=lambda: clear_all(self))
+        self.clear_button.place(x=300,y=125)
         # About
         self.about_button = tk.Button(self.root,text="?", command=about)
         self.about_button.place(x=360, y=125)
